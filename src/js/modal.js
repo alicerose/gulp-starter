04aec4d327120e557da06b2fb38d5bc28fd6e3a1
Takehiro Kato <takehiro.kato@teamfactory.jp>
2018-06-28 13:14:30 +0900

    general modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,29 +1,46 @@
 // modal
-var defer = $.Deferred();
+var current;
+var defer    = $.Deferred();
 var duration = 300;
 
-var modalLoad = () => {
+var modalInit = () => {
   $('[data-modal]').each(function(){
     $(this).appendTo('body');
   })
-  $('body').append('<div data-overlay></div>');
 }
 
 var showOverlay = (target, method) => {
-  $('[data-overlay]').attr('data-overlay', target).attr('data-onclick', method);
-  $('[data-overlay]').fadeIn(duration);
+  $('body').append('<div data-overlay></div>');
+  $('[data-overlay]').attr('data-overlay', target).attr('data-onclick', method).fadeIn(duration);
 }
 
 var modalLaunch = (target, overlay, callback) => {
-  showOverlay(target, 'noClose');
+  console.log(target);
+  showOverlay(target, 'close');
   $('[data-modal=' + target + ']').addClass('-visible');
+  scrollLock();
 }
 
 var modalClose = (target) => {
+  console.log(target);
   var method = $('[data-overlay]').attr('data-onclick');
   if(method == 'close'){
     $('[data-modal]').removeClass('-visible');
     $('[data-overlay="' + target + '"]').fadeOut(duration);
+    setTimeout(function() {
+      $('[data-overlay="' + target + '"]').remove();
+      scrollLock('unlock');
+    }, duration);
+  }
+}
+
+var scrollLock = (method) => {
+  if(method !== 'unlock'){
+    current = $(document).scrollTop();
+    $('body').addClass('js-modal-scrollLock').css('top', current*-1);
+  } else {
+    $('body').removeClass('js-modal-scrollLock').css('top', '');
+    $(document).scrollTop(current);
   }
 }
 
@@ -35,6 +52,10 @@ $(document).on('click', '[data-overlay]', function(){
   modalClose($(this).data('overlay'));
 })
 
+$(document).on('click', '[data-modal-close]', function(){
+  modalClose($(this).parents('[data-modal]').data('modal'));
+})
+
 $(window).on('load',function(){
-  modalLoad();
+  modalInit();
 });
