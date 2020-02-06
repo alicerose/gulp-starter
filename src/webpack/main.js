143fc72e3b71bdd26a25a59fb71f9150d79cef46
Takehiro Kato <czk@seeds-9.com>
2020-02-06 20:10:56 +0900

    add webpack examples

---
+++ src/webpack/main.js
@@ -0,0 +1,8 @@
+import {exportHoge} from "./test";
+import {SOME_NUMBER} from "./constants/common";
+
+import $ from "jquery";
+
+exportHoge(SOME_NUMBER);
+const text = $('header h1').text();
+alert(text);
