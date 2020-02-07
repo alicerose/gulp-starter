7d09875fdedf6941f0e41ede59e56e621cdca788
Takehiro Kato <czk@seeds-9.com>
2020-02-07 15:33:25 +0900

    configure lint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,20 +5,21 @@ module.exports = {
     'plugin:prettier/recommended'
   ],
   env: {
-    'browser': true,
-    'es6': true,
-    'jquery': true,
-    'node': true,
+    browser: true,
+    es6: true,
+    es2020: true,
+    jquery: true,
+    node: true,
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-extra-semi': 'warn',
     'no-unused-vars': 'warn',
+    'no-multiple-empty-lines': ['error', {max: 1}],
     'prettier/prettier': 'error',
   },
   parserOptions: {
-    'ecmaVersion': 6,
-    'sourceType': 'module',
+    sourceType: 'module',
   }
 }
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,11 +1,10 @@
 module.exports = {
-  'trailingComma': 'es5',
-  'tabWidth': 2,
-  'useTabs': false,
-  'semi': true,
-  'singleQuote': true,
-  'indent': ['error', 2],
-  'no-multiple-empty-lines': ['error', {max: 1}],
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  indent: ['error', 2],
   arrowParens: 'avoid',
   endOfLine: 'crlf',
 }
