5cfb1be0c45617044cea1692a8ea7f84b070e964
Takehiro Kato <czk@seeds-9.com>
2020-02-07 14:53:42 +0900

    add eslint/prettier

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:prettier/recommended"
+  ],
+  env: {
+    "browser": true,
+    "es6": true,
+    "jquery": true,
+    "node": true,
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    "no-extra-semi": "warn",
+    "prettier/prettier": "error",
+  },
+  parserOptions: {
+    "sourceType": "module"
+  }
+}
---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  "trailingComma": "es5",
+  "tabWidth": 2,
+  "useTabs": false,
+  "semi": true,
+  "singleQuote": true,
+  "indent": ['error', 2],
+  'no-multiple-empty-lines': ['error', {max: 1}],
+  arrowParens: "avoid",
+  endOfLine: "crlf",
+}
